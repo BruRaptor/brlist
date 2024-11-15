227bd47e8ac9e150f839d75003d0589d4181bb30
Bruno Ribeiro <brunoribeirokyo@gmail.com>
2024-11-15 17:34:51 -0300

    Project created

---
+++ src/main/java/com/brurpator/brlist/BrlistApplication.java
@@ -0,0 +1,13 @@
+package com.brurpator.brlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brurpator/brlist/BrlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brurpator.brlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
